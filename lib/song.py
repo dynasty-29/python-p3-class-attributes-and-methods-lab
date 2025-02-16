51549cb4229a07eff0516e7cc57f4e57c700e1fb
SonnieCodes <mgathoni.gathoni9@gmail.com>
2025-02-16 10:16:47 +0300

    object class

--- lib/song.py
+++ lib/song.py
@@ -1,2 +1,52 @@
 class Song:
-    pass
+    count = 0
+    genres = []
+    artists = []
+    genre_count = {}
+    artist_count = {}
+
+    def __init__(self, name, artist, genre):
+        self.name = name
+        self.artist = artist
+        self.genre = genre
+        # song count
+        self.add_song_to_count()
+        # Add genre and artist to respective lists
+        self.add_to_genres()
+        self.add_to_artists()
+        # Update genre and artist counts
+        self.add_to_genre_count()
+        self.add_to_artist_count()
+
+    @classmethod
+    def add_song_to_count(cls):
+        cls.count += 1
+
+    def add_to_genres(self):
+        if self.genre not in Song.genres:
+            Song.genres.append(self.genre)
+
+    def add_to_artists(self):
+        if self.artist not in Song.artists:
+            Song.artists.append(self.artist)
+
+    def add_to_genre_count(self):
+        if self.genre in Song.genre_count:
+            Song.genre_count[self.genre] += 1
+        else:
+            Song.genre_count[self.genre] = 1
+
+    def add_to_artist_count(self):
+        if self.artist in Song.artist_count:
+            Song.artist_count[self.artist] += 1
+        else:
+            Song.artist_count[self.artist] = 1
+
+    @classmethod
+    def show_song(cls):
+        print(f"Total Songs Created: {cls.count}")
+        print(f"Genres: {cls.genres}")
+        print(f"Artists: {cls.artists}")
+        print(f"Genre Count: {cls.genre_count}")
+        print(f"Artist Count: {cls.artist_count}")
+
